f41fb928e6e778b0fa9a37d9be35b13b16eefa72
Nat <nat.wrw@gmail.com>
2024-04-18 12:52:49 +0700

    Update Navbar

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
 const NavBar: React.FC = () => {
   const router = useRouter();
+  const pathName = usePathname();
 
   const isActive = (path: string): string => {
-    return router.pathname === path ? 'bg-gray-900' : '';
+    return pathName === path ? 'bg-gray-900' : '';
   };
 
   return (
@@ -20,20 +21,26 @@ const NavBar: React.FC = () => {
         <span className="text-white text-lg font-bold">ABV Tools</span>
       </div>
       <div className="flex">
-        <Link href="/">
-          <a className={`text-gray-300 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium ${isActive('/')}`}>
+        <Link
+          href="/"
+          className={`text-gray-300 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium ${isActive('/')}`}>
+          
             Home
-          </a>
+          
         </Link>
-        <Link href="/abv-dilution">
-          <a className={`text-gray-300 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium ${isActive('/abv-dilution')}`}>
+        <Link
+          href="/abv-dilution"
+          className={`text-gray-300 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium ${isActive('/abv-dilution')}`}>
+          
             ABV Dilution
-          </a>
+          
         </Link>
-        <Link href="/abv-calculator">
-          <a className={`text-gray-300 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium ${isActive('/abv-calculator')}`}>
+        <Link
+          href="/abv-calculator"
+          className={`text-gray-300 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium ${isActive('/abv-calculator')}`}>
+          
             ABV Calculator
-          </a>
+          
         </Link>
       </div>
     </nav>
